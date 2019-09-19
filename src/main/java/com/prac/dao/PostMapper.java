af8005adf63e4a0b91c23e0055c14ff3e6131bd3
hoik92 <hoik92@nate.com>
2019-09-19 17:06:30 +0900

    mybatis mapper 이름 변경

--- src/main/java/com/prac/dao/PostMapper.java
+++ src/main/java/com/prac/dao/PostMapper.java
@@ -6,7 +6,7 @@ import org.apache.ibatis.annotations.*;
 import java.util.List;
 
 @Mapper
-public interface BoardMapper {
+public interface PostMapper {
 
     @Select("select * from posts")
     public List<Posts> findAll();
--- src/main/java/com/prac/service/PostServiceImpl.java
+++ src/main/java/com/prac/service/PostServiceImpl.java
@@ -1,6 +1,6 @@
 package com.prac.service;
 
-import com.prac.dao.BoardMapper;
+import com.prac.dao.PostMapper;
 import com.prac.domain.posts.Posts;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,30 +11,30 @@ import java.util.List;
 public class PostServiceImpl implements PostService {
 
     @Autowired
-    BoardMapper boardMapper;
+    PostMapper postMapper;
 
     @Override
     public List<Posts> findAll() {
-        return boardMapper.findAll();
+        return postMapper.findAll();
     }
 
     @Override
     public Posts findById(int id) {
-        return boardMapper.findById(id);
+        return postMapper.findById(id);
     }
 
     @Override
     public void insert(Posts post) {
-        boardMapper.insert(post);
+        postMapper.insert(post);
     }
 
     @Override
     public void update(Posts post) {
-        boardMapper.update(post);
+        postMapper.update(post);
     }
 
     @Override
     public void delete(int id) {
-        boardMapper.delete(id);
+        postMapper.delete(id);
     }
 }
